fb525e88bb0ddaa52202abe3e12dbf55af9df2ef
Gabriel <gabe@gabefraser.com>
2021-10-21 20:51:33 +1100

    Integrated downloading and file hash check

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module toontown-offline-launcher
 
 go 1.17
+
+require github.com/dustin/go-humanize v1.0.0
--- main.go
+++ main.go
@@ -1,14 +1,21 @@
 package main
 
 import (
+	"crypto/md5"
+	"encoding/hex"
 	"encoding/json"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
 	"regexp"
 	"runtime"
+	"strings"
 	"time"
+
+	"github.com/dustin/go-humanize"
 )
 
 type File struct {
@@ -21,6 +28,23 @@ type Files struct {
 	List []File
 }
 
+type DownloadTotal struct {
+	Progress uint64
+	Filename string
+}
+
+func (dt *DownloadTotal) Write(p []byte) (int, error) {
+	n := len(p)
+	dt.Progress += uint64(n)
+	dt.PrintProgress(dt.Filename)
+	return n, nil
+}
+
+func (dt DownloadTotal) PrintProgress(filename string) {
+	fmt.Printf("\r%s", strings.Repeat(" ", 35))
+	fmt.Printf("\rDownloading %s... %s complete", filename, humanize.Bytes(dt.Progress))
+}
+
 func (f *Files) addFile(file File) []File {
 	f.List = append(f.List, file)
 	return f.List
@@ -30,21 +54,91 @@ func (f *Files) getFiles() []File {
 	return f.List
 }
 
+func (f *File) getFullFilePath() string {
+	if len(f.Path) > 0 {
+		sprintf := "%s/%s"
+		path := f.Path
+
+		if getRuntimePlatform() == "windows" {
+			sprintf = "%s\\%s"
+			path = strings.Replace(path, "/", "\\", -1)
+		}
+
+		return fmt.Sprintf(sprintf, path, f.Name)
+	} else {
+		return f.Name
+	}
+}
+
 func (f *File) getFilePath() string {
-	return fmt.Sprintf("%s/%s", f.Path, f.Name)
+	if len(f.Path) > 0 {
+		path := f.Path
+
+		if getRuntimePlatform() == "windows" {
+			path = strings.Replace(path, "/", "\\", -1)
+		}
+
+		return path
+	}
+
+	return ""
+}
+
+func (f *File) getURLPath() string {
+	if len(f.Path) > 0 {
+		path := f.Path
+
+		if getRuntimePlatform() == "windows" {
+			path = strings.Replace(path, "/", "\\", -1)
+		}
+
+		return fmt.Sprintf("%s/%s/%s.bz2", patchingURL, f.Path, f.Name)
+	}
+
+	return fmt.Sprintf("%s/%s.bz2", patchingURL, f.Name)
 }
 
 var patcher map[string]interface{}
 var files Files
-var patchingURL = "https://releases.toontownoffline.net/%s.json"
+var patchingURL = "https://releases.toontownoffline.net"
+var patcherURL = patchingURL + "/%s.json"
 
 func main() {
-	url := fmt.Sprintf(patchingURL, getRuntimePlatform())
+	url := fmt.Sprintf(patcherURL, getRuntimePlatform())
 
 	parsePatcher(url)
 
+	patchFiles()
+
+	bootGame()
+}
+
+func bootGame() {
+	fmt.Println("Booting the game...")
+}
+
+func patchFiles() {
 	for _, file := range files.getFiles() {
-		fmt.Println(file.getFilePath())
+		if _, err := os.Stat(file.getFullFilePath()); os.IsNotExist(err) {
+			err := downloadFile(file)
+			if err != nil {
+				panic(err)
+			}
+		} else if os.IsExist(err) {
+			hash, err := getFileHash(file)
+			if err != nil {
+				panic(err)
+			}
+
+			fmt.Println(fmt.Sprintf("File: %s, current hash: %s, patcher hash: %s", file.Name, hash, file.Hash))
+
+			if hash != file.Hash {
+				err := downloadFile(file)
+				if err != nil {
+					panic(err)
+				}
+			}
+		}
 	}
 }
 
@@ -104,6 +198,64 @@ func generateFiles(m map[string]interface{}) {
 			}
 		}
 	}
+}
+
+func downloadFile(file File) error {
+	if _, err := os.Stat(file.getFilePath()); os.IsNotExist(err) {
+		os.MkdirAll(file.getFilePath(), os.ModePerm)
+	}
+
+	out, err := os.Create(file.getFullFilePath() + ".tmp")
+	if err != nil {
+		return err
+	}
+
+	resp, err := http.Get(file.getURLPath())
+	if err != nil {
+		out.Close()
+		return err
+	}
+	defer resp.Body.Close()
+
+	counter := &DownloadTotal{
+		Filename: file.Name,
+	}
+	if _, err := io.Copy(out, io.TeeReader(resp.Body, counter)); err != nil {
+		out.Close()
+		return err
+	}
+
+	fmt.Println("")
+
+	out.Close()
+
+	if err := os.Rename(file.getFullFilePath()+".tmp", file.getFullFilePath()); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func getFileHash(file File) (string, error) {
+	var md5Hash string
+
+	curFile, err := os.Open(file.getFullFilePath())
+	if err != nil {
+		return md5Hash, err
+	}
+
+	defer curFile.Close()
+
+	hash := md5.New()
+
+	if _, err := io.Copy(hash, curFile); err != nil {
+		return md5Hash, err
+	}
+
+	hashInBytes := hash.Sum(nil)[:16]
+	md5Hash = hex.EncodeToString(hashInBytes)
+
+	return md5Hash, nil
 }
 
 func getRuntimePlatform() string {
